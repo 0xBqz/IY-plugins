23bfc751756cc4cead63c6b56bb6ac531347a444
baba <97072588+0xBqz@users.noreply.github.com>
2022-09-06 17:47:07 -0700

    Update Skid Manager.lua

--- Skid Manager.lua
+++ Skid Manager.lua
@@ -1,43 +1,18 @@
---[[Tools]]
+
 local HttpService = game:GetService("HttpService")
 local skids = {}
 
---[[Plufin json file]]
-
 if isfile("Skids.json") then
-    if HttpService:JSONDecode(readfile("Skids.json")).skids ~= nil then skids = json.skids else skids = {} end
-else
-    writefile("Skids.json","")
-end
-
-function AddSkid(name)
-    local repet
-    for i,v in pairs(skids) do
-        if v == name then
-            repet = true
-        end
-    end
-
-    if repet then
-        notify("Error "..name.." is not a valid skid","the user "..name.." is already on the skidlist")
-    else
-        notify("Succes! added "..name.."to the skidlist","added "..name.." to the skidlist. your settings will save")
-        table.insert(skids,name)
-    end
-
-    writefile("Skids.json", HttpService:JSONEncode({skids = skids}))
-end
-
-local function chatnotify(text,color)
-    game.StarterGui:SetCore("ChatMakeSystemMessage",{
-        Text = text, Color = color
-    })
+    if HttpService:JSONDecode(readfile("Skids.json")) ~= nil then skids = HttpService:JSONDecode(readfile("Skids.json")) else skids = {} end else writefile("Skids.json","{}")
 end
 
 local function notifyskid(name)
     for _,plrs in pairs(skids) do
         if plrs == name.Name then
-            chatnotify("SKID DETECTED!!!: " ..plrs,Color3.fromRGB(205, 84, 75))
+            game.StarterGui:SetCore("ChatMakeSystemMessage",{
+                ["Text"] = "SKID DETECTED!!!: " ..plrs,
+                ["Color"] = Color3.fromRGB(205, 84, 75)
+            })
         end
     end
 end
@@ -50,12 +25,6 @@ game.Players.PlayerAdded:Connect(function(plr)
     notifyskid(plr)
 end)
 
-game.Players.PlayerRemoving:Connect(function(plr)
-    chatnotify("the skid: "..plr..", has left the experience",Color3.fromRGB(205, 84, 75))
-end)
-
-
---[[Plugin table]]
 local Plugin = {
     ["PluginName"] = "Skid's manager",
     ["PluginDescription"] = "helps u managing the skids, added by you.",
@@ -66,17 +35,27 @@ local Plugin = {
             ["Aliases"] = {"addskid"},
             ["Function"] = function(args,speaker)
                 for _,plr in pairs(getPlayer(args[1],speaker)) do
-                    AddSkid(plr)
+                    if not FindInTable(skids, plr) and Players.LocalPlayer.Name ~= plr then
+                        notify("Succes! added "..plr.."to the skidlist","added "..plr.." to the skidlist. your settings will save")
+                        table.insert(skids,plr)
+
+                        writefile("Skids.Json",HttpService:JSONEncode(skids))
+                    else
+                        notify("Invalid argument",plr.." is not a valid player to add in the skidlist.")
+                    end
                 end
             end
         },
         ["skidlist"] = {
             ["ListName"] = "skidlist",
             ["Description"] = "notifies all skids.",
-            ["Aliases"] = {""},
+            ["Aliases"] = {},
             ["Function"] = function(args,speaker)
                 notify("Succes! notified all skids","all saved skids are notified in the chat")
-                chatnotify(HttpService:JSONEncode(skids))
+                game.StarterGui:SetCore("ChatMakeSystemMessage",{
+                    ["Text"]  =  HttpService:JSONEncode(skids),
+                    ["Color"] = Color3.fromRGB(205, 84, 75)
+                })
             end
         },
     }
